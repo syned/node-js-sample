0429fc67024245e4657b0d5875cd2b7e800d5543
Denys Nosulenko <synedoff@gmail.com>
2013-08-15 23:09:45 +0200

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Maxim! I"m just simple node.js application running on Heroku, follow me :-)');
 });
 
 var port = process.env.PORT || 5000;
